2bf924eb2db17a4646295238377bf9a07b50f0cf
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2021-02-15 21:32:32 +0100

    Add files via upload

---
+++ usr/lib/enigma2/python/Plugins/Extensions/KodiLite/scripts/script.module.XXXresolver/lib/XXXresolver/__init__.py
@@ -0,0 +1,7 @@
+sources = ["shemalemovie", "xhamster", "youporn", "pornhub", "h2porn", "youporn",\
+"redtube", "yourlust", "bravotube", "anyporn", "drtuber", "tnaflix"]
+#includes youtube_dl working resolvers
+
+
+
+
